f0dfa054ea29ff158feaf735315422060fab3184
Owain Lewis <owain@owainlewis.com>
2016-11-28 19:33:16 +0000

    Basics working

--- src/Language/Seven/Core.hs
+++ src/Language/Seven/Core.hs
@@ -1,59 +1,110 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Language.Seven.Core
-    ( runInstr
-    , run
+    ( run
+    , push
+    , pop
+    , peek
     ) where
 
-import Control.Lens
 import Control.Monad.State
 import Control.Monad.Except
+import Control.Lens
+
+import qualified Data.Map as M
 
 data Value = Word String
-           | Integer Int
-  deriving ( Show )
+           | Number Int
+           | Procedure String [Value]
+  deriving ( Eq, Show )
 
-data VM = VM {
-    pointer :: Int
-  , input :: [Value]
-  , output :: [Value]
-} deriving ( Show )
+data Stack = Stack {
+    -- | The runtime stack that holds the current program state
+    _runtime :: [Value]
+    -- | The global environment used to storage user defined procs
+  , _env :: [(String, [Value])]
+} deriving ( Eq, Show )
 
-type ProgramError = String
+makeLenses ''Stack
 
-pushVM :: VM -> Value -> VM
-pushVM vm@(VM _ _ output) x = vm { output = x : output }
+newtype VM a = VM { runVM :: ExceptT ProgramError (StateT Stack IO) a }
+    deriving ( Functor
+             , Applicative
+             , Monad
+             , MonadState Stack
+             , MonadError ProgramError
+             , MonadIO
+             )
 
-push :: Value -> StateT VM IO (Maybe Value)
-push v = do
-  vm <- get
-  modify (flip pushVM v)
-  return Nothing
+data ProgramError =
+      StackUnderflowException
+    | RuntimeException String
 
-pop :: StateT VM IO (Maybe Value)
+instance Show ProgramError where
+  show StackUnderflowException = "Error: Stack underflow"
+  show (RuntimeException e) = e
+
+environment :: [(String, VM ())]
+environment = [ ("+", binOp (+))
+              ]
+
+
+-- | Push an item onto the runtime stack
+--
+--   liftIO . print $  "PUSH"
+push :: Value -> VM ()
+push x = do
+    modify f
+    where f (Stack output e) = Stack (x:output) e
+
+-- | Pop an item off the stack
+--
+pop :: VM Value
 pop = do
-  vm <- get
-  put vm
-  return (Just (Word "OK"))
+  stack <- get
+  case stack of
+    (Stack [] _) -> throwError StackUnderflowException
+    s@(Stack (x:xs) _) -> do
+          put $ s { _runtime = xs }
+          return x
 
--- pushPop :: StateT VM Identity (Either String Value)
--- pushPop = do
---   push $ Integer 10
---   push $ Integer 20
---   x <- pop
---   return x
+peek :: VM (Maybe Value)
+peek = do
+  stack <- get
+  case stack of
+    (Stack [] _) ->
+        return Nothing
+    (Stack (x:xs) _) ->
+        return . pure $ x
 
--- λ> runState (pushOutput (Word "HELLO")) (mkVM [])
--- ((),VM {pointer = 0, input = [], output = [Word "HELLO"]})
+noop :: VM ()
+noop = return ()
 
-data Instruction = Push Value | Pop | GoTo Int
+-- | ------------------------------------------------------------------------
 
-mkVM :: VM
-mkVM = VM { pointer = 0, input = [], output = [] }
+-- | Apply a binary operation to two elements on the stack
+--
+binOp :: (Int -> Int -> Int) -> VM ()
+binOp op = do
+  x <- pop
+  y <- pop
+  case (x,y) of
+    (Number x1, Number y1) -> push $ Number (x1 `op` y1)
+    _ -> throwError $ RuntimeException "Expecting two integers"
 
-runInstr :: Instruction -> t -> t
-runInstr (Push v) vm = vm
+-- | ------------------------------------------------------------------------
 
-run :: Value -> t -> Instruction
-run (Integer n) vm = Push (Integer n)
-run _ _ = error "Not implemented"
+symTab "+" = binOp (+)
+symTab _ = throwError $ RuntimeException "Unknown word"
+
+-- | Run a series of steps on the stack
+--
+run :: VM a -> Stack -> IO (Either ProgramError a, Stack)
+run f s = runStateT (runExceptT (runVM f)) s
+
+exec :: Foldable a => a Value -> Stack -> IO (Either ProgramError (), Stack)
+exec program stack = run (forM_ program eval) stack
+    where eval (Number n) = push $ (Number n)
+          eval (Word w) = symTab w
+
+
