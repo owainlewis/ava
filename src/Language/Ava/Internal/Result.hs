64c911f9054d24870cdde2209fed61b4f68e9a10
Owain Lewis <owain@owainlewis.com>
2017-08-03 08:36:52 +0100

    Update Result.hs

--- src/Language/Ava/Internal/Result.hs
+++ src/Language/Ava/Internal/Result.hs
@@ -8,24 +8,20 @@ module Language.Ava.Internal.Result
 
 import Data.Bifunctor (bimap, Bifunctor)
 
------------------------------------------------------------
 data Result e a
   = Failure e
   | Success a
   deriving (Eq, Ord, Show, Read)
 
------------------------------------------------------------
 instance Functor (Result e) where
   fmap f (Success a) = Success (f a)
   fmap f (Failure e) = Failure e
   {-# INLINE fmap #-}
 
------------------------------------------------------------
 instance Bifunctor Result where
   bimap f g = result (Failure . f) (Success . g)
   {-# INLINE bimap #-}
 
------------------------------------------------------------
 instance Applicative (Result e) where
   pure = Success
   {-# INLINE pure #-}
@@ -33,20 +29,17 @@ instance Applicative (Result e) where
   Success _ <*> Failure e = Failure e
   Success f <*> Success x = Success (f x)
   {-# INLINE <*> #-}
-
------------------------------------------------------------
+  
 instance Monad (Result e) where
   return = Success
   Success m >>= k = k m
   Failure e >>= _ = Failure e
   {-# INLINE >>= #-}
 
------------------------------------------------------------
 instance Foldable (Result a) where
   foldMap _ (Failure _) = mempty
   foldMap f (Success x) = f x
 
------------------------------------------------------------
 result :: (a -> c) -> (b -> c) -> Result a b -> c
 result f g (Failure x) = (f x)
 result f g (Success x) = (g x)
